a1f16dc436d26621f636851c95d5d94a40855bd3
Alan-Ventura999 <djhjdhuiir@gmail.com>
2026-05-20 21:59:19 -0600

    Diseño mejorado del portfolio UI

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("themeBtn");
+
+button.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
